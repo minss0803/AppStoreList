df932207de9cc600235b6c8ec2b8785082ca1d54
minss0803 <minss0803@naver.com>
2019-12-03 00:11:31 +0900

    Initial Commit

---
+++ HandMade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandMade
+//
+//  Created by 민쓰 on 03/12/2019.
+//  Copyright © 2019 민쓰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
